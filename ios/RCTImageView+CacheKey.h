f64eae3f2f7735e2a82447830e3d3d68323f24c2
sdbarlow <sethdbarlow@gmail.com>
2024-08-19 13:26:53 -0500

    another fix

--- ios/RCTImageView+CacheKey.h
+++ ios/RCTImageView+CacheKey.h
@@ -1,9 +1,16 @@
-#import <React/RCTImageView.h>
-#import "RCTImageView.h"
+#ifndef RCTImageView_CacheKey_h
+#define RCTImageView_CacheKey_h
+
+#import <React/RCTView.h>
 #import "IFKCacheable.h"
+
+@interface RCTImageView : RCTView
+@end
 
 @interface RCTImageView (CacheKey) <IFKCacheable>
 
 - (nonnull NSString *)cacheKey;
 
 @end
+
+#endif /* RCTImageView_CacheKey_h */
--- ios/RCTImageView+CacheKey.m
+++ ios/RCTImageView+CacheKey.m
@@ -1,4 +1,3 @@
-#import <React/RCTImageView.h>
 #import "RCTImageView+CacheKey.h"
 #import <React/RCTImageSource.h>
 #import "NSArray+FilterMapReduce.h"
@@ -7,15 +6,20 @@
 
 - (nonnull NSString *)cacheKey
 {
-  return [self.imageSources reduce:^id(NSString *key, RCTImageSource *source, int idx) {
-    return [NSString stringWithFormat:
-            @"%@(%@_%f_%@)",
-            key,
-            [NSValue valueWithCGSize:source.size],
-            source.scale,
-            source.request.URL.absoluteString];
+    // Assuming imageSources and resizeMode are properties of RCTImageView
+    // If they're not, you may need to adjust this implementation
+    NSArray<RCTImageSource *> *imageSources = [self valueForKey:@"imageSources"];
+    NSInteger resizeMode = [[self valueForKey:@"resizeMode"] integerValue];
     
-  } init:[NSString stringWithFormat:@"%ld", (long)self.resizeMode]];
+    return [imageSources reduce:^id(NSString *key, RCTImageSource *source, int idx) {
+        return [NSString stringWithFormat:
+                @"%@(%@_%f_%@)",
+                key,
+                [NSValue valueWithCGSize:source.size],
+                source.scale,
+                source.request.URL.absoluteString];
+        
+    } init:[NSString stringWithFormat:@"%ld", (long)resizeMode]];
 }
 
 @end
